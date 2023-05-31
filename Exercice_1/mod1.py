4588c398902245243c0283984d812ba0e7c42f2c
alexandrephl <alex.panhaleux@orange.fr>
2023-05-31 16:04:39 -0400

    Exercice 1

--- Exercice_1/mod1.py
+++ Exercice_1/mod1.py
@@ -17,8 +17,29 @@ def aire_poly_rect(limite_inf, limite_sup, coef_p1, coef_p2, coef_p3, coef_p4, n
     
     return sum_aire_rect
 
-def error_integ(aire_anal, aire_rect):
-    return abs((aire_anal-aire_rect)/aire_anal)*100
 
-print(aire_poly_rect(-2, 2, 2,3,4,5,10))
-print(error_integ(29.12, 29.33))
+# On calcule l'aire sous la courbe I selon la méthode exacte (méthode analytique)
+def aire_poly_exacte(limite_inf, limite_sup, coef_p1, coef_p2, coef_p3, coef_p4):
+    resultat = coef_p1 * (limite_sup-limite_inf) + coef_p2 * ((limite_sup**2-limite_inf**2)/2) + coef_p3 * ((limite_sup**3-limite_inf**3)/3) + coef_p4 * ((limite_sup**4-limite_inf**4)/4)
+    return resultat
+
+def error_integ(aire_anal, aire_rect):
+    return abs(aire_anal-aire_rect)
+
+def evolution_erreur(limite_inf, limite_sup, coef_p1, coef_p2, coef_p3, coef_p4):
+    error_integr = []
+    liste_nb_seg = []
+
+    for i in range (1, 100):
+        nb_seg = i
+        aire_poly_rectangle = aire_poly_rect(limite_inf, limite_sup, coef_p1, coef_p2, coef_p3, coef_p4, nb_seg)
+        aire_poly_anal = aire_poly_exacte(limite_inf, limite_sup, coef_p1, coef_p2, coef_p3, coef_p4)
+
+        error_integr.append(error_integ(aire_poly_anal, aire_poly_rectangle))
+        liste_nb_seg.append(i)
+
+    plt.plot(error_integr)
+    plt.show()
+    return error_integr
+
+
