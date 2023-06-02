5e7c2c07cfe754aba9c79c9bad90b116da3e0b77
alexandrephl <alex.panhaleux@orange.fr>
2023-06-02 17:32:43 -0400

    avancement tomporel

--- Exercice_2/mod1.py
+++ Exercice_2/mod1.py
@@ -10,10 +10,11 @@ nb_segments_x = 45
 nb_segment_y = 65
 XC = 5
 YC = 5
-coeff_diffusion = 0.7
+coeff_diffusion = 0.000173
 amplitude_point_chaud = 50
 sigma_point_chaud = 5
 temperature_atmosphere = 20
+instant = 0
 
 # Fonction qui génère une grille de calcul (de forme carrée)
 # On créé un vecteur X
@@ -26,7 +27,7 @@ def grille(longueur, hauteur, nb_segments_x, nb_segments_y):
     grid['Y'] = y
     plt.figure()
     plt.plot(grid['X'], yv.transpose(), linestyle='-', color='grey', linewidth=0.5)
-    plt.plot(xv, grid['Y'], linestyle='-', color='grey', linewidth=0.5)
+    plt.plot(xv, grid['Y'], linestyle='-', color='grey', linewidth=1)
     return grid
 
 # Sur la grille de calcul, on trace le contour en couleur pour le champ de température
@@ -55,13 +56,28 @@ def Calcul_RHS(grid, coeff_diffusion, Temp):
     for index_X in range(1, len(grid['X'])-1):
         for index_Y in range(1, len(grid['Y'])-1):
             RHS[index_X, index_Y] = coeff_diffusion*((Temp[index_X+1, index_Y] - 2*Temp[index_X, index_Y] + Temp[index_X-1, index_Y])/(grid['X'][1]**2) + (Temp[index_X, index_Y+1] - 2*Temp[index_X, index_Y] + Temp[index_X, index_Y-1])/(grid['Y'][1]**2))
+    print(RHS)
     return RHS
 
 
-#def avancement_temporel():
+def avancement_temporel(Temp, grid, coeff_diffusion, instant_avancement):
+    dt = (0.25 * (grid['X'][1]**2))/coeff_diffusion
+    print(dt)
+    global instant 
+    instant += dt 
+
+    if instant < instant_avancement :
+        NewTemp = Temp + dt*Calcul_RHS(grid, coeff_diffusion, Temp)
+        avancement_temporel(NewTemp, grid, coeff_diffusion, instant_avancement)
+    else:
+        Affichage_temp(grid, Temp)
+        print(Temp)
+        return Temp
+    
 
 
 Grille=grille(longueur, hauteur, nb_segments_x, nb_segment_y)
 Temp_init = solution_initiale(Grille, amplitude_point_chaud, sigma_point_chaud, XC, YC, temperature_atmosphere)
-Affichage_temp(Grille, Temp_init)
-Calcul_RHS(Grille, coeff_diffusion, Temp_init)
+#Affichage_temp(Grille, Temp_init)
+#Calcul_RHS(Grille, coeff_diffusion, Temp_init)
+avancement_temporel(Temp_init, Grille, coeff_diffusion, 10)
