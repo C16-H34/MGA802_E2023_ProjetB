4f13a16f7bb4cb7f80294d44a77f219b296c9e5e
C16-H34 <133154131+C16-H34@users.noreply.github.com>
2023-06-05 23:29:47 -0400

    Modifs Exercise 1
    Presque terminé, reste à corriger la liste pour l'erreur d'intégration avec les fonctions Python de base

--- Exercice_1/main.py
+++ Exercice_1/main.py
@@ -9,16 +9,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import timeit
 
-# On importe la fonction pour calculer l'aire sous
+# Importation des fonctions codées en python de base
 from mod1 import aire_poly_rect
 from mod1 import erreur_integ
 from mod1 import aire_poly_exacte
 from mod1 import evolution_erreur
-
+from mod1 import temps_exec_evolution_erreur
+# Importation des fonctions codées avec numpy
+from mod1 import np_tableau_nb_segments
+from mod1 import np_aire_poly_rect
 from mod1 import np_evolution_erreur
+from mod1 import np_temps_execution
 
 
-# Définitions des constantes
+'''DÉFINITION DES CONSTANTES'''
 # Ont définie les limites inférieures et supérieures où l'on évalue la courbe
 limite_inf = -2
 limite_sup = 2
@@ -27,30 +31,48 @@ coefs = [2, 3, 4, 5]
 # Nombre de segments avec lesquels ont veut diviser la plage définie par les limites inférieures et supérieures
 # afin d'appliquer la méthode des rectangles pour évaluer l'aire sous la courbe
 nb_seg = 100
+tableau_np_nb_seg = np_tableau_nb_segments(nb_seg)
 
 
+'''EXÉCUTION DES FONCTIONS ET CALCULS'''
+#On calcule l'aire sous la courbe I avec la méthode des rectangles et on affiche le résulat
 resultat_methode_rect = aire_poly_rect(limite_inf, limite_sup, coefs, nb_seg)
-resultat_methode_exacte = aire_poly_exacte(limite_inf, limite_sup, coefs)
-erreur_integration = erreur_integ(resultat_methode_rect, resultat_methode_exacte)
 print(f'\nAvec la méthode des rectangles et {nb_seg} segments, l\'aire sous la courbe est {resultat_methode_rect}')
+#On calcule l'aire sous la courbe I avec la méthode exacte et on affiche le résulat
+resultat_methode_exacte = aire_poly_exacte(limite_inf, limite_sup, coefs)
 print(f'Avec la méthode exacte, l\'aire sous la courbe est {resultat_methode_exacte}')
+#On calcule l'erreur d'intégration entre la méthode des rectangles et la méthode exacte et on affiche le résulat
+erreur_integration = erreur_integ(resultat_methode_rect, resultat_methode_exacte)
 print(f'L\'erreur d\'intégration est de {erreur_integration}')
 
-evolution_error = evolution_erreur(limite_inf, limite_sup, coefs, nb_seg)
- 
+#On évalue la convergence de l'erreur d'intégration avec un nombre croissant de segments, jusqu'à atteindre la valeur spécifiée "nb_seg"
+#On affiche ensuite un graphe démontrant l'erreur d'intégration en fonction du nombre de segments
+convergence_erreur = evolution_erreur(limite_inf, limite_sup, coefs, nb_seg)
+
+
+'''ÉVALUATION DU TEMPS D'EXÉCUTION DES FONCTIONS'''
 # Évaluation du temps d'exécution du script, avec les fonctions codées en python de base
 # On affiche à l'utilisateur la valeur calculée
-temps_execution_fonctions_base = timeit.timeit(lambda: aire_poly_rect(limite_inf, limite_sup, coefs, nb_seg), number = 1)
-temps_execution_fonctions_base = temps_execution_fonctions_base + timeit.timeit(lambda: aire_poly_exacte(limite_inf, limite_sup, coefs), number = 1)
-temps_execution_fonctions_base = temps_execution_fonctions_base + timeit.timeit(lambda: erreur_integ(resultat_methode_rect, resultat_methode_exacte), number = 1)
-temps_execution_fonctions_base = temps_execution_fonctions_base + timeit.timeit(lambda: evolution_erreur(limite_inf, limite_sup, coefs, nb_seg), number = 1)
-print(f"\nTemps requis pour exécuter le script avec les fonctions codées en python de base: {temps_execution_fonctions_base} secondes")
+temps_execution_fonction_base = timeit.timeit(lambda: aire_poly_rect(limite_inf, limite_sup, coefs, nb_seg), number = 1)
+print(f"\nTemps requis pour calculer l'intégrale de la courbe I avec la fonction codée en python de base: {temps_execution_fonction_base} secondes")
 
 # Évaluation du temps d'exécution du script, avec les fonctions vectorisées et codées avec numpy
 # On affiche à l'utilisateur la valeur calculée
-temps_execution_avec_numpy = timeit.timeit(lambda: aire_poly_rect(limite_inf, limite_sup, coefs, nb_seg), number = 1)
-temps_execution_fonctions_base = temps_execution_fonctions_base + timeit.timeit(lambda: aire_poly_exacte(limite_inf, limite_sup, coefs), number = 1)
-temps_execution_fonctions_base = temps_execution_fonctions_base + timeit.timeit(lambda: erreur_integ(resultat_methode_rect, resultat_methode_exacte), number = 1)
-temps_execution_fonctions_base = temps_execution_fonctions_base + timeit.timeit(lambda: np_evolution_erreur(limite_inf, limite_sup, coefs, nb_seg), number = 1)
-print(f"\nTemps requis pour exécuter le script avec les fonctions avec numpy: {temps_execution_avec_numpy} secondes")
+temps_execution_avec_numpy = timeit.timeit(lambda: np_tableau_nb_segments(nb_seg), number = 1)
+temps_execution_avec_numpy = temps_execution_avec_numpy + timeit.timeit(lambda: np_aire_poly_rect(limite_inf, limite_sup, coefs, tableau_np_nb_seg), number = 1)
+print(f"\nTemps requis pour calculer l'intégrale de la courbe I avec la fonction codée avec numpy: {temps_execution_avec_numpy} secondes")
 
+
+'''COMPARAISON DE L'ERREUR DES 2 TYPES DE FONCTIONS EN FONCTION DU TEMPS D'EXÉCUTION'''
+temps_exec_erreur_integr = temps_exec_evolution_erreur(limite_inf, limite_sup, coefs, nb_seg)
+temps_exec_erreur_integr_np = np_temps_execution(limite_inf, limite_sup, coefs, tableau_np_nb_seg)
+tableau_erreur_np = np_evolution_erreur(limite_inf, limite_sup, coefs, tableau_np_nb_seg)
+new_tableau_erreur_np = np.delete(tableau_erreur_np,0)
+
+#plt.plot(temps_exec_erreur_integr, erreur_integration, label = "Fonctions Python de base",color='b')
+plt.plot(temps_exec_erreur_integr_np, new_tableau_erreur_np, label = "Fonction Python avec numpy",color='r')
+plt.title("Erreur d'intégration en fonction du temps d'exécution",loc="center")
+plt.xlabel("Temps d'exécution en secondes")
+plt.ylabel("Erreur d'intégration")
+plt.legend
+plt.show()
--- Exercice_1/mod1.py
+++ Exercice_1/mod1.py
@@ -7,6 +7,7 @@
 # Importation de bibliothèques additionnelles
 import numpy as np
 import matplotlib.pyplot as plt
+import timeit
 
 '''FONCTIONS CODÉES SEULEMENT AVEC PYTHON DE BASE'''
 
@@ -60,10 +61,32 @@ def evolution_erreur(limite_inf, limite_sup, coefs, nb_seg):
     plt.show()
     return erreur_integr
 
+def temps_exec_evolution_erreur(limite_inf, limite_sup, coefs, nb_seg):
+    temps_exec_erreur_integr = [float()] #Initialisation de la liste contenant chaque valeur du temps d'exécution de la fonction d'erreur d'intégration
+
+    for i in range (1, nb_seg): #On calcule l'erreur pour un nombre croissant de segments, de 1 à la limite spécifiée
+        temps = timeit.timeit(lambda: aire_poly_rect(limite_inf, limite_sup, coefs, i), number=1)
+        temps += timeit.timeit(lambda: aire_poly_exacte(limite_inf, limite_sup, coefs), number=1)
+        #On calcule l'erreur et on ajoute sa valeur à la liste
+        temps += timeit.timeit(lambda: erreur_integ(aire_poly_exacte(limite_inf, limite_sup, coefs), aire_poly_rect(limite_inf, limite_sup, coefs, i)), number=1)
+        temps_exec_erreur_integr.append(temps)
+    temps_exec_erreur_integr.pop(0)
+    return(temps_exec_erreur_integr)
+
 
 '''FONCTIONS CODÉES AVEC NUMPY'''
 
+# Fonction qui créé un tableau ndarray avec le nombre de segments spécifiés dans le main.
+# Retourne un tableau ndarray contenant le nombre croissant (à partir de 1) du nombre de segments.
+def np_tableau_nb_segments(nb_seg):
+    np_nb_seg = np.arange(1, nb_seg + 1, dtype=float)
+    np_nb_seg = np_nb_seg.reshape(len(np_nb_seg), 1)
+    return np_nb_seg
+
+# Fonction qui calcule l'aire sous la courbe I d'une fonction polynomiale de degré 3 avec la méthode des rectangles
+# Retourne l'aire sous la courbe
 def np_aire_poly_rect(limite_inf, limite_sup, coefs, np_nb_seg):
+
     sum_aire_rect = 0
     np_aire_rect = np.ones(np_nb_seg.shape)
     increm = (limite_sup - limite_inf)/np_nb_seg
@@ -79,8 +102,8 @@ def np_aire_poly_rect(limite_inf, limite_sup, coefs, np_nb_seg):
 
     return np_aire_rect
 
-
-
+# Fonction qui calcule l'aire sous la courbe I avec la méthode exacte, en faisant appel à des fonctions numpy.
+# Retourne un tableau de la même taille
 def np_aire_poly_exacte(limite_inf, limite_sup, coefs, np_nb_seg):
     np_aire_exacte = np.ones(np_nb_seg.shape)
     np_aire_exacte = np_aire_exacte*(coefs[0] * (limite_sup - limite_inf) + coefs[1] * ((limite_sup**2-limite_inf**2)/2) + coefs[2] * ((limite_sup**3-limite_inf**3)/3) + coefs[3] * ((limite_sup**4-limite_inf**4)/4))
@@ -89,21 +112,25 @@ def np_aire_poly_exacte(limite_inf, limite_sup, coefs, np_nb_seg):
 
 # Fonction qui calcule l'erreur d'intégration pour le nombre de segments spécifié. La commence a calculer à partir de
 # 1 segment, puis augmente le nombre de segments de 1 jusqu'à atteindre la limite spécifiée.
-# Retourne un graphique illustrant l'erreur en fonction du nombre de segments utilisés dans la fonction de calcul de
-# l'aire avec la méthode des rectangles. On cherche à vérifier s'il y a convergence plus le nombre de rectangles augmente.
+# Retourne un tableau illustrant l'erreur d'intégration en fonction du nombre de segments
 def np_evolution_erreur(limite_inf, limite_sup, coefs, np_nb_seg):
-    #tableau_erreur = np.arange(1, nb_seg + 1, dtype=float)
-    #tableau_erreur = tableau_erreur.reshape(len(tableau_erreur), 1)
-
     np_aire_poly_rectangle = np_aire_poly_rect(limite_inf, limite_sup, coefs, np_nb_seg)
     np_aire_poly_analytique = np_aire_poly_exacte(limite_inf, limite_sup, coefs, np_nb_seg)
 
     tableau_erreur = (np.abs(np_aire_poly_rectangle-np_aire_poly_analytique)/np_aire_poly_analytique)*100
     return tableau_erreur
 
-np_nb_seg = np.arange(1, 10 + 1, dtype=float)
-np_nb_seg = np_nb_seg.reshape(len(np_nb_seg), 1)
-print(np_aire_poly_exacte(-2, 2, [2, 3, 4, 5], np_nb_seg))
+def np_temps_execution(limite_inf, limite_sup, coefs, np_nb_seg):
+    #tableau_erreur = np_evolution_erreur(limite_inf, limite_sup, coefs, np_nb_seg)
+    temps_exec_erreur_integr_np = [float()] #Initialisation de la liste contenant chaque valeur du temps d'exécution de la fonction d'erreur d'intégration
 
-print(np_aire_poly_rect(-2, 2, [2, 3, 4, 5], np_nb_seg))
-print(np_evolution_erreur(-2, 2, [2, 3, 4, 5], np_nb_seg))
+    for i in range (1, len(np_nb_seg)): #On calcule l'erreur pour un nombre croissant de segments, de 1 à la limite spécifiée
+        temps = timeit.timeit(lambda: np_aire_poly_exacte(limite_inf, limite_sup, coefs, np_nb_seg), number=1)
+        temps += timeit.timeit(lambda: np_aire_poly_exacte(limite_inf, limite_sup, coefs, np_nb_seg), number=1)
+        #On calcule l'erreur et on ajoute sa valeur à la liste
+        temps += timeit.timeit(lambda: np_evolution_erreur(limite_inf, limite_sup, coefs, np_nb_seg), number=1)
+        temps_exec_erreur_integr_np.append(temps)
+
+    temps_exec_erreur_integr_np.pop(0)
+
+    return temps_exec_erreur_integr_np
